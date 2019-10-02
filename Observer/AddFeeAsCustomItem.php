0f9b562b8ac6534df76a8ab02d790faaa6eb9914
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2019-10-02 09:38:01 +0200

    Adjusting additional product for amazon pay

--- Observer/AddFeeAsCustomItem.php
+++ Observer/AddFeeAsCustomItem.php
@@ -16,23 +16,32 @@ class AddFeeAsCustomItem implements \Magento\Framework\Event\ObserverInterface
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        if(!$this->configuration->isEnabled()){
+        if (!$this->configuration->isEnabled()) {
             return $this;
         }
 
         $cart = $observer->getEvent()->getCart();
+        $payment = $cart->getSalesModel()->getDataUsingMethod('payment');
         $totals = $cart->getSalesModel()->getDataUsingMethod('totals');
 
-        if(!isset($totals[\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE])){
+        $bulkGoodsFee = 0;
+
+        if (isset($totals[\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE])) {
+            $bulkGoodsTotal = $totals[\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE];
+
+            if ($bulkGoodsTotal->getValue()) {
+                $bulkGoodsFee = $bulkGoodsTotal->getValue();
+            }
+        }
+
+        if (!$bulkGoodsFee && $payment->getMethod() == \Magento\Paypal\Model\Config::METHOD_EXPRESS) {
+            $bulkGoodsFee = $cart->getSalesModel()->getDataUsingMethod(\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE);
+        }
+
+        if (!$bulkGoodsFee) {
             return $this;
         }
 
-        $bulkGoodsTotal = $totals[\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE];
-
-        if(!$bulkGoodsTotal->getValue()){
-            return $this;
-        }
-
-        $cart->addCustomItem($this->configuration->getLabel(), 1, $bulkGoodsTotal->getValue(), $bulkGoodsTotal->getCode());
+        $cart->addCustomItem($this->configuration->getLabel(), 1, $bulkGoodsFee, \MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE);
     }
 }
