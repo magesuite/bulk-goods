ef7306e773fd064b6bf7259d3e110f822024aba5
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2024-09-30 16:23:28 +0200

    fix: [DOG-1922] bulk fee double invoiced

--- Model/Total/Invoice/BulkGoods.php
+++ Model/Total/Invoice/BulkGoods.php
@@ -1,13 +1,11 @@
 <?php
 
+declare(strict_types=1);
 namespace MageSuite\BulkGoods\Model\Total\Invoice;
 
 class BulkGoods extends \Magento\Sales\Model\Order\Invoice\Total\AbstractTotal
 {
-    /**
-     * @var \MageSuite\BulkGoods\Model\BulkGoods
-     */
-    protected $bulkGoods;
+    protected \MageSuite\BulkGoods\Model\BulkGoods $bulkGoods;
 
     public function __construct(
         \MageSuite\BulkGoods\Model\BulkGoods $bulkGoods,
@@ -20,13 +18,23 @@ class BulkGoods extends \Magento\Sales\Model\Order\Invoice\Total\AbstractTotal
     public function collect(\Magento\Sales\Model\Order\Invoice $invoice)
     {
         $order = $invoice->getOrder();
-        $invoice->setBulkGoodsFee($order->getBulkGoodsFee());
-        $invoice->setBulkGoodsTax($order->getBulkGoodsTax());
+        $bulkGoodsFee = (float) $order->getData(\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_CODE);
+        $bulkGoodsFeeInvoiced = (float) $order->getData(\MageSuite\BulkGoods\Model\BulkGoods::BULK_GOODS_FEE_INVOICED_CODE);
 
+        if ($bulkGoodsFee > 0 && $bulkGoodsFeeInvoiced === 0.0) {
+            $this->applyBulkGoodsFee($invoice, $bulkGoodsFee);
+        }
+
+        return $this;
+    }
+
+    protected function applyBulkGoodsFee(\Magento\Sales\Api\Data\InvoiceInterface $invoice, float $bulkGoodsFee):void
+    {
+        $order = $invoice->getOrder();
+        $invoice->setBulkGoodsFee($bulkGoodsFee);
+        $order->setBulkGoodsFeeInvoiced($bulkGoodsFee);
         $bulkGoodsFeeExclTax = $this->bulkGoods->getOrderFeeExclTax($order);
         $invoice->setGrandTotal($invoice->getGrandTotal() + $bulkGoodsFeeExclTax);
         $invoice->setBaseGrandTotal($invoice->getBaseGrandTotal() + $bulkGoodsFeeExclTax);
-
-        return $this;
     }
 }
--- Test/Integration/Model/Total/Invoice/BulkGoodsFeeTest.php
+++ Test/Integration/Model/Total/Invoice/BulkGoodsFeeTest.php
@@ -1,4 +1,6 @@
 <?php
+
+declare(strict_types=1);
 namespace MageSuite\BulkGoods\Test\Integration\Model\Total\Invoice;
 
 /**
@@ -7,21 +9,16 @@ namespace MageSuite\BulkGoods\Test\Integration\Model\Total\Invoice;
  */
 class BulkGoodsFeeTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @var \MageSuite\BulkGoods\Test\Integration\Helper\Order
-     */
-    protected $orderHelper;
-
-    /**
-     * @var \Magento\Sales\Model\Service\InvoiceService
-     */
-    protected $invoiceService;
+    protected ?\MageSuite\BulkGoods\Test\Integration\Helper\Order $orderHelper = null;
+    protected ?\Magento\Sales\Model\Service\InvoiceService $invoiceService = null;
+    protected ?\Magento\Catalog\Api\ProductRepositoryInterface $productRepository = null;
 
     public function setUp(): void
     {
         $objectManager = \Magento\TestFramework\ObjectManager::getInstance();
         $this->orderHelper = $objectManager->get(\MageSuite\BulkGoods\Test\Integration\Helper\Order::class);
         $this->invoiceService = $objectManager->get(\Magento\Sales\Model\Service\InvoiceService::class);
+        $this->productRepository = $objectManager->get(\Magento\Catalog\Api\ProductRepositoryInterface::class);
     }
 
     /**
@@ -37,10 +34,10 @@ class BulkGoodsFeeTest extends \PHPUnit\Framework\TestCase
      * @magentoConfigFixture current_store tax/classes/shipping_tax_class 2
      * @magentoConfigFixture current_store tax/defaults/country DE
      * @magentoConfigFixture current_store shipping/origin/country_id DE
-     * @magentoDataFixture loadProducts
-     * @magentoDataFixture loadTaxRates
+     * @magentoDataFixture MageSuite_BulkGoods::Test/Integration/_files/tax_rates.php
+     * @magentoDataFixture MageSuite_BulkGoods::Test/Integration/_files/products.php
      */
-    public function testItAddsBulkGoodsFeeWithCorrectTaxToInvoice()
+    public function testItAddsBulkGoodsFeeWithCorrectTaxToInvoice():void
     {
         $expectedFeeWithTax = 10;
         $expectedFeeWithoutTax = 8.4;
@@ -76,10 +73,10 @@ class BulkGoodsFeeTest extends \PHPUnit\Framework\TestCase
      * @magentoConfigFixture current_store tax/classes/shipping_tax_class 2
      * @magentoConfigFixture current_store tax/defaults/country DE
      * @magentoConfigFixture current_store shipping/origin/country_id DE
-     * @magentoDataFixture loadProducts
-     * @magentoDataFixture loadTaxRates
+     * @magentoDataFixture MageSuite_BulkGoods::Test/Integration/_files/tax_rates.php
+     * @magentoDataFixture MageSuite_BulkGoods::Test/Integration/_files/products.php
      */
-    public function testItAddsBulkGoodsFeeWithCorrectTaxToInvoiceWithDifferentShippingCountry()
+    public function testItAddsBulkGoodsFeeWithCorrectTaxToInvoiceWithDifferentShippingCountry():void
     {
         $expectedFee = 10;
         $expectedTax = 1.87;
@@ -93,13 +90,53 @@ class BulkGoodsFeeTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($expectedTax, $invoice->getTaxAmount());
     }
 
-    public static function loadTaxRates()
+    /**
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation enabled
+     * @magentoConfigFixture current_store bulk_goods/general/is_enabled 1
+     * @magentoConfigFixture current_store bulk_goods/general/fee 10
+     * @magentoConfigFixture current_store carriers/flatrate/price 0
+     * @magentoConfigFixture current_store general/country/default DE
+     * @magentoConfigFixture current_store tax/calculation/based_on shipping
+     * @magentoConfigFixture current_store tax/calculation/shipping_includes_tax 1
+     * @magentoConfigFixture current_store tax/classes/shipping_tax_class 2
+     * @magentoConfigFixture current_store tax/defaults/country DE
+     * @magentoConfigFixture current_store shipping/origin/country_id DE
+     * @magentoDataFixture MageSuite_BulkGoods::Test/Integration/_files/tax_rates.php
+     * @magentoDataFixture MageSuite_BulkGoods::Test/Integration/_files/products.php
+     */
+    public function testItAddsBulkGoodsFeeToOrderWithPartialInvoice()
     {
-        require __DIR__ . '/../../../_files/tax_rates.php';
-    }
+        $quantities = [];
+        $expectedBulkGoodsFee = 10;
+        $expectedBulkGoodsTax = 1.6;
+        $firstInvoiceExpectedTax = 11.1;
+        $secondInvoiceExpectedTax = 9.5;
+        $orderExpectedTax = 20.6;
+        $orderExpectedTotal = 129;
 
-    public static function loadProducts()
-    {
-        require __DIR__ . '/../../../_files/products.php';
+        $product = $this->productRepository->get('product');
+        $product->setTaxClassId(2)->save();
+
+        $order = $this->orderHelper->createOrder('DE', 2, $product);
+        $orderItems = $order->getItems();
+        $orderItem = array_shift($orderItems);
+        $quantities[$orderItem->getId()] = 1;
+
+        $firstInvoice = $this->invoiceService->prepareInvoice($order, $quantities);
+        $firstInvoice->register()->save();
+        $this->assertEquals($expectedBulkGoodsFee, $firstInvoice->getBulkGoodsFee());
+        $this->assertEquals($expectedBulkGoodsTax, $firstInvoice->getBulkGoodsTax());
+        $this->assertEquals($firstInvoiceExpectedTax, $firstInvoice->getTaxAmount());
+
+        $secondInvoice = $this->invoiceService->prepareInvoice($order, $quantities);
+        $secondInvoice->register()->save();
+        $this->assertEquals(0, $secondInvoice->getBulkGoodsFee());
+        $this->assertEquals(0, $secondInvoice->getBulkGoodsTax());
+        $this->assertEquals($secondInvoiceExpectedTax, round($secondInvoice->getTaxAmount(), 2));
+
+        $this->assertEquals($orderExpectedTax, $order->getBaseTaxAmount());
+        $this->assertEquals($orderExpectedTotal, $order->getBaseGrandTotal());
     }
 }
